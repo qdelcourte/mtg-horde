6c44bc381084059f18cd5136c21179fe8b07d9d7
Quentin Delcourte <delcourte.quentin@hotmail.com>
2024-08-10 22:13:46 +0200

    Add the original horde deck

--- decks/index.js
+++ decks/index.js
@@ -1,7 +1,9 @@
 import zombie_hard from './zombie_hard.json';
 import zombie_medium from './zombie_medium.json';
+import zombie_original from './zombie_original.json';
 
 export default {
 	zombie_hard,
-	zombie_medium
+	zombie_medium,
+	zombie_original
 };
