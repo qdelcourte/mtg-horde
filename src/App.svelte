3087cfa51531b28bd8caf0e0e58efe007e670d24
Quentin Delcourte <delcourte.quentin@hotmail.com>
2024-08-11 09:20:58 +0200

    Add debug page to compare distribution modes

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,24 @@
 	import './app.postcss';
 	import Board from './components/Board.svelte';
 	import GithubCorner from './components/GithubCorner.svelte';
+	import CompareDistribution from './CompareDistribution.svelte';
 	import { game as G } from './game.svelte';
 
 	$effect(() => {
 		if (G.state.ctx.gameover) alert(G.state.ctx.gameover.winner);
 	});
+
+	let page = $state(document.location.hash);
 </script>
 
+<svelte:window on:hashchange={() => page = document.location.hash}/>
+	
 <GithubCorner />
 
-<div id="app" class="h-full">
-	<Board />
-</div>
+{#if page === '#distribution'}
+	<CompareDistribution/>
+{:else}
+	<div id="app" class="h-full">
+		<Board />
+	</div>
+{/if}
---
+++ src/CompareDistribution.svelte
@@ -0,0 +1,88 @@
+<script>
+	import Icon from "@iconify/svelte";
+    import { loadDeck, isTokenCard } from "./gameHelpers.js";
+    import zombie_original from '../decks/zombie_original.json';
+
+    const distributionModes = ['geometric', 'geometric_boosted', 'random'];
+
+    let NB_GENERATION = $state(15);
+    let NB_SURVIVORS = $state(4);
+    let TOKEN_PROPORTION = $state(0.6);
+    let generations = $state({});
+    let now = $state(Date.now());
+
+    $effect(() => {
+        if (now) {
+            distributionModes.forEach((m) => {
+                generations[m] = Array.from({length: NB_GENERATION}, () => loadDeck(zombie_original, NB_SURVIVORS, TOKEN_PROPORTION, m));;
+            });
+        }
+    });
+</script>
+
+<div class="p-4">
+    <h1 class="text-center text-2xl font-bold">Compare distribution modes</h1>
+
+    <div class="mb-2 flex items-center justify-between">
+        <div>
+            <label for="nb-generations">Nb generations</label>
+            <input id="nb-generations" bind:value={NB_GENERATION} type="number">
+    
+            <label for="nb-survivors">Nb survivors</label>
+            <input id="nb-survivors" bind:value={NB_SURVIVORS} type="number">
+    
+            <label for="token-proportion">Token proportion</label>
+            <input id="token-proportion" bind:value={TOKEN_PROPORTION} type="number" max="1" min="0" step="0.1">
+
+            <button onclick={() => now = Date.now()} class="relative align-middle transition-all disabled:opacity-50 w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs border border-gray-900 text-gray-900 hover:opacity-50 focus:ring focus:ring-gray-300 active:opacity-[0.85]" type="button">
+                <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"><Icon icon="mdi:refresh" width="20"/></span>
+            </button>
+        </div>
+        <div>
+            <p class="underline">Legend:</p>
+            <ul>
+                <li class="flex items-center gap-2"><div class="card token"></div> Token card</li>
+                <li class="flex items-center gap-2"><div class="card"></div> Non token card</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="grid grid-cols-3 gap-2">
+        {#each distributionModes as mode}
+            <div class="p-4 bg-white shadow-md bg-clip-border rounded-xl">
+                <h2 class="m-1">{@render chip(mode)}</h2>
+                {#each generations[mode] as cards, i}
+                    <h3 class="underline">Generation {i+1}</h3>
+                    <div class="flex">
+                        {#each cards as card}
+                            <div class="card" class:token={isTokenCard(card)}></div>
+                        {/each}
+                    </div>
+                {/each}
+            </div>
+        {/each}
+    </div>
+</div>
+
+{#snippet chip(text)}
+    <div class="relative grid select-none items-center whitespace-nowrap rounded-lg bg-gray-500 py-2 px-2 font-sans text-sm font-bold uppercase text-white">
+        <span class="">{text}</span>
+    </div>
+{/snippet}
+
+<style lang="postcss">
+    :global(body) {
+        @apply bg-slate-100;
+    }
+
+    .card {
+        width: 1rem;
+        height: 1.5rem;
+        @apply bg-green-600 m-[1px];
+    }
+
+    .card.token {
+        width: .5rem;
+        @apply bg-red-600;
+    }
+</style>
